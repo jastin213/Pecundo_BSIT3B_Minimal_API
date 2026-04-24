36281265553d36f0dc13db444cb5dfc7bb787e41
jastin213 <jastinpecundo06@gmail.com>
2026-04-24 16:59:08 +0800

    final

---
+++ ProductManagementAPI/Controllers/ProductController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProductManagementAPI.Data;
+using ProductManagementAPI.Models;
+
+namespace ProductManagementAPI.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ProductController : ControllerBase
+	{
+		private readonly ApplicationDbContext _context;
+
+		public ProductController(ApplicationDbContext context)
+		{
+			_context = context;
+		}
+
+		// GET: api/product (WITH Category + Supplier)
+		[HttpGet]
+		public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+		{
+			return await _context.Products
+				.Include(p => p.Category)
+				.Include(p => p.Supplier)
+				.ToListAsync();
+		}
+
+		// GET: api/product/1
+		[HttpGet("{id}")]
+		public async Task<ActionResult<Product>> GetProduct(int id)
+		{
+			var product = await _context.Products
+				.Include(p => p.Category)
+				.Include(p => p.Supplier)
+				.FirstOrDefaultAsync(p => p.ProductId == id);
+
+			if (product == null)
+				return NotFound();
+
+			return product;
+		}
+
+		// POST: api/product
+		[HttpPost]
+		public async Task<ActionResult<Product>> CreateProduct(Product product)
+		{
+			_context.Products.Add(product);
+			await _context.SaveChangesAsync();
+
+			return Ok(product);
+		}
+
+		// PUT: api/product/1
+		[HttpPut("{id}")]
+		public async Task<IActionResult> UpdateProduct(int id, Product product)
+		{
+			if (id != product.ProductId)
+				return BadRequest();
+
+			_context.Entry(product).State = EntityState.Modified;
+			await _context.SaveChangesAsync();
+
+			return Ok();
+		}
+
+		// DELETE: api/product/1
+		[HttpDelete("{id}")]
+		public async Task<IActionResult> DeleteProduct(int id)
+		{
+			var product = await _context.Products.FindAsync(id);
+
+			if (product == null)
+				return NotFound();
+
+			_context.Products.Remove(product);
+			await _context.SaveChangesAsync();
+
+			return Ok();
+		}
+	}
+}
--- ProductManagementAPI/obj/Debug/net10.0/ProductManagementAPI.AssemblyInfo.cs
+++ ProductManagementAPI/obj/Debug/net10.0/ProductManagementAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductManagementAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f913759011a7b6287329f0542187ac7ce999c2a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a30ab676f8db0dbbd7d62b844297609221bb0a13")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductManagementAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductManagementAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
